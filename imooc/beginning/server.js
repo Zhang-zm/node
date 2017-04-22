15d2fe150654a9382ba6738e7e50ee9f90c9372b
Zhang_zm <763876185@qq.com>
2017-04-22 15:46:12 +0800

    .
    .

---
+++ imooc/beginning/server.js
@@ -0,0 +1,14 @@
+
+var http=require('http');
+
+var server = http.createServer(function(req,res){
+
+    res.writeHead(200,{'Content-Type':'text-plain'});
+
+    res.end('Hello Nodejs\n');
+
+});
+
+server.listen(1337,'127.0.0.1');
+
+console.log('Server is runnig at http://127.0.0.1:1337');
